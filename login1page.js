e9bc3b319e0095f4d03022086ae694495aa2f162
YuvraajSalaria <74704624+YuvraajSalaria@users.noreply.github.com>
2021-06-13 13:36:07 +0530

    Add files via upload

---
+++ login1page.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDzsV1Lle6pG2qxD9mou6Zbd8HqnDRfdGE",
+    authDomain: "kwitter-cc71c.firebaseapp.com",
+    databaseURL: "https://kwitter-cc71c-default-rtdb.firebaseio.com",
+    projectId: "kwitter-cc71c",
+    storageBucket: "kwitter-cc71c.appspot.com",
+    messagingSenderId: "38444060383",
+    appId: "1:38444060383:web:05b90c587575582dbba36a",
+    measurementId: "G-ZVS5HEGN09"
+  };
